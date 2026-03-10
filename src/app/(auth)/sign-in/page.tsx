77fda5c57561bf085796bfb076f11839d121db44
Czar-16 <anoopjha2003@gmail.com>
2026-03-10 12:19:34 +0530

    sign-in frontend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,51 @@
-"use client"
+"use client";
+import { useDebounceValue } from "usehooks-ts";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+import Link from "next/link";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios from 'axios'
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
-  );
-}
+const page = () => {
+  const [username, setUsername] = useState("");
+
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+  const router = useRouter();
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsernameUnique =async()=>{
+      if(debouncedUsername){
+        setIsCheckingUsername(true)
+        setUsernameMessage("")
+        try {
+          axios.get("/api/")
+          
+          
+        } catch (error) {
+          
+        }
+      }
+    }
+  }, [debouncedUsername]);
+  return <div>page</div>;
+};
+
+export default page;
